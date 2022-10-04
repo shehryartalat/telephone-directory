acd39b08c144b42eee0285be78f20e76ec97dde2
shehryartalat <shehryartalal41@gmail.com>
2022-10-04 09:56:22 +0500

    Public-telephone-directory

--- src/components/List.js
+++ src/components/List.js
@@ -12,15 +12,13 @@ export default function List(props) {
             <div className='row container'>
                 {
                     value.map((data) => {
-                        return <div key={data.value} className='mx-5 col-lg-3 col-md-6 col-sm-12 container bg-dark text-white m-2' style={{borderRadius:'15px'}}>
+                        return <div key={data.value} className='mx-5 col-lg-3 col-md-6 col-sm-12 container bg-dark text-white m-2' style={{ borderRadius: '15px' }}>
                             {data.Title}<br />
                             {data.Name}<br />
                             <hr></hr>
-                            {data.Phone}<br/>
+                            {data.Phone}<br />
                             {data.Categories}
                         </div>
-
-
                     }
                     )
                 }
